384c4bc563728f57f90f6ae6bfcd0b8a94091c67
brennank14 <brennank14@gmail.com>
2020-07-21 12:20:21 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ActiveRecord::Base
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(fiction non-fiction)}
+
+
+    def validate(post)
+        validates :post, exclusion: { in:%w("Won't Believe", "Secret", "Top [number]", "Guess")}
+    end
 end
